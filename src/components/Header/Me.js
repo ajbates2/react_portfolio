df0b12add039f4005b0db9094c7ad0de74498aca
AJ Bates <aj.bates2.92@gmail.com>
2021-01-14 13:43:46 -0600

    small tweaks and fixed typo

--- src/components/Header/Me.js
+++ src/components/Header/Me.js
@@ -3,7 +3,7 @@ import transparent from '../../images/transparent_headshot.png'
 export default function Me() {
     return (
         <div className='headshot_container'>
-            <img className='headshot' src={transparent} alt='AJ Bates II headshot' />
+            <img className='headshot' src={transparent} alt='AJ looking to side and smiling' />
         </div>
     )
 }

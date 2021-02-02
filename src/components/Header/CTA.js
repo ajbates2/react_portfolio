761bddc7b8ed96c2751c3a429ebdbf1f0c7e47d8
AJ Bates <aj.bates2.92@gmail.com>
2021-02-01 18:14:09 -0600

    resized picture

--- src/components/Header/CTA.js
+++ src/components/Header/CTA.js
@@ -2,7 +2,6 @@ import CTAButtons from './CTAButtons';
 import CTAIcons from './CTAIcons';
 
 export default function CTA() {
-
 	return (
 		<div className='header_cta'>
 			<h1 className='my_name'>AJ Bates</h1>

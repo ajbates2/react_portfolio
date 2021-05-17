0d36881e07e371312c974475f39df2db0006107f
AJ Bates <aj.bates2.92@gmail.com>
2021-05-17 12:50:36 -0500

    fixed some responsive and layout issues

--- src/components/About/AboutContainer.js
+++ src/components/About/AboutContainer.js
@@ -11,22 +11,23 @@ export default function AboutContainer(props) {
 				<TechBlock />
 			</div>
 			<div className={`about ${props.background}`}>
-				<div>
-				<p>
-					Having worked in a restaurant and in the service industry
-					for essentially my whole life, I work great in a team, and
-					especially great in high stress situations. I can't wait to
-					work with a great team of programmers and developers to
-					apply my knowledge to a different challenge everyday.
-				</p>
-				<p>
-					My love for technology is what ultimately pushed me to get
-					into web development. I enjoy video games (built multiple
-					PC's), movies, and music. When I'm not coding you might find
-					me bartending (I love beer), or going to see some live music
-					(whenever we're allowed). Just like every other millennial I
-					collect vinyl and shoes.
-				</p>
+				<div className='bio_div'>
+					<p>
+						Having worked in a restaurant and in the service
+						industry for essentially my whole life, I work great in
+						a team, and especially great in high stress situations.
+						I can't wait to work with a great team of programmers
+						and developers to apply my knowledge to a different
+						challenge everyday.
+					</p>
+					<p>
+						My love for technology is what ultimately pushed me to
+						get into web development. I enjoy video games (built
+						multiple PC's), movies, and music. When I'm not coding
+						you might find me bartending (I love beer), or going to
+						see some live music (whenever we're allowed). Just like
+						every other millennial I collect vinyl and shoes.
+					</p>
 				</div>
 			</div>
 		</section>
